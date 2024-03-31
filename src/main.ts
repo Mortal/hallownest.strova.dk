8a8cb527c06ee48da45c856df782f823211bbfe3
Mathias Rav <m@git.strova.dk>
2024-03-31 23:01:19 +0200

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
-import './style.css';
-import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import "./style.css";
+import { Map } from "ol";
+import WebGLTileLayer from "ol/layer/WebGLTile";
+import GeoTIFF from "ol/source/GeoTIFF";
+import { defaults } from "ol/interaction/defaults";
 
-const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+const source = new GeoTIFF({
+  sources: [{ url: "/hallownest.tif" }],
+  convertToRGB: true,
+});
+
+(window as any).map = new Map({
+  interactions: defaults({ pinchRotate: false }),
+  target: "map",
+  layers: [new WebGLTileLayer({ source })],
+  view: source.getView(),
+  controls: [],
 });
